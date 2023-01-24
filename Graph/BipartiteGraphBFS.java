10247bb8f80b5ebbe6e615326ff069dbb761cd9f
Rahul B <93582933+RahulBisht001@users.noreply.github.com>
2023-01-24 07:21:28 +0530

    Update BipartiteGraphBFS.java

--- Graph/BipartiteGraphBFS.java
+++ Graph/BipartiteGraphBFS.java
@@ -1,1 +1,40 @@
+// Check For Bipartite Graph using BFS
+class Solution
+{
+    public boolean isBipartite(int v, ArrayList<ArrayList<Integer>>adj)
+    {
+        // 1. Linear Graph with No Cycle are
+        //    Always Bipartite
+        // 2. Any Graph with Even Cycle Length 
+        //    can also be Bipaertite
+        return checkBipartite(v, adj);
+    }
+    
+    public boolean checkBipartite(int v, ArrayList<ArrayList<Integer>>adj){
+        int[] color = new int[v];
+        for(int i=0;i<v;++i)
+            color[i] = -1;
+            
+        Queue<Integer>q = new LinkedList<>();   
+        for(int i=0; i < v;++i){
+            if(color[i] == -1){
+                q.add(i);
+                color[i] = 0;
+                
+                while(!q.isEmpty()){
+                    int node = q.remove();
+                    for(Integer idx: adj.get(node)){
+                        if(color[idx] == -1){
+                            color[idx] = 1 - color[node];
+                            q.add(idx);
+                        }
+                        else if(color[idx] == color[node]) 
+                            return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
 
