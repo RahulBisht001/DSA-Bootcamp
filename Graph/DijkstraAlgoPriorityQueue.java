e476bacc742531bf6c85785845e8929ce7fa8091
Rahul B <93582933+RahulBisht001@users.noreply.github.com>
2023-01-25 10:11:48 +0530

    Create DijkstraAlgoPriorityQueue.java

---
+++ Graph/DijkstraAlgoPriorityQueue.java
@@ -0,0 +1,33 @@
+
+class Solution
+{
+   static int[] dijkstra(int v, ArrayList<ArrayList<ArrayList<Integer>>> adj, int s)
+    {
+       PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        int[] distance = new int[v];
+
+        for (int i = 0; i < v; ++i)
+            distance[i] = Integer.MAX_VALUE;
+
+        distance[s] = 0;
+        pq.add(new int[] { 0, s });
+
+        while (!pq.isEmpty()) {
+            int dis = pq.peek()[0];
+            int node = pq.peek()[1];
+            pq.remove();
+
+            for (int i = 0; i < adj.get(node).size(); ++i) {
+                int edgeWeight = adj.get(node).get(i).get(1);
+                int adjNode = adj.get(node).get(i).get(0);
+
+                if (dis + edgeWeight < distance[adjNode]) {
+                    distance[adjNode] = dis + edgeWeight;
+                    pq.add(new int[] { distance[adjNode], adjNode });
+                }
+            }
+        }
+
+        return distance;
+    }
+}
